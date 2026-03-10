f3a9774bc6d8b5c5226d1aa7268b212449bbb0ee
Daria <diconna@gmail.com>
2026-03-10 16:57:04 +0700

    add eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,9 +5,29 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+    {
+        rules: {
+            "@typescript-eslint/await-thenable": "warn",
+            "@typescript-eslint/no-explicit-any": "warn",
+            "@typescript-eslint/no-misused-promises": "warn",
+            "@typescript-eslint/no-unused-vars": "warn",
+            "@typescript-eslint/return-await": "warn",
+            "curly": "warn",
+            "eqeqeq": "warn",
+            "import/no-cycle": "warn",
+            "import/order": "warn",
+            "newline-before-return": "warn",
+            "no-console": "warn",
+            "no-undef": "off",
+            "no-unreachable": "warn",
+            "no-unused-vars": "off",
+            "object-shorthand": "warn",
+            "prefer-const": "warn",
+            "react/jsx-no-useless-fragment": "warn",
+            "require-await": "warn"
+        }
+    },
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
